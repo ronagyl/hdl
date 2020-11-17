215ea0e145200395ff0eaac92a560703d96d3612
Laszlo Nagy <laszlo.nagy@analog.com>
2020-11-17 13:31:01 +0000

    ad9081_fmca_ebz/zcu102: WIP make PMOD outputs tristate

--- projects/ad9081_fmca_ebz/zcu102/system_top.v
+++ projects/ad9081_fmca_ebz/zcu102/system_top.v
@@ -84,12 +84,12 @@ module system_top  #(
   output [1:0]  txen,
 
   // PMOD0
-  output        pmod0_0_1_PA_ON,
-  output        pmod0_4_2_TR,
+  inout         pmod0_0_1_PA_ON,
+  inout         pmod0_4_2_TR,
   output        pmod0_1_3_MOSI,
-  output        pmod0_5_4_TX_LOAD,
+  inout         pmod0_5_4_TX_LOAD,
   input         pmod0_2_5_MISO,
-  output        pmod0_6_6_RX_LOAD,
+  inout         pmod0_6_6_RX_LOAD,
   output        pmod0_3_7_SCLK,
   inout         pmod0_7_8_SCL,
   // PMOD1
@@ -98,9 +98,9 @@ module system_top  #(
   output        pmod1_1_3_CSB3,
   output        pmod1_5_4_CSB4,
   output        pmod1_2_5_CSB5,
-  output        pmod1_6_6_5V_CTRL,
+  inout         pmod1_6_6_5V_CTRL,
   inout         pmod1_3_7_SDA,
-  output        pmod1_7_8_PWR_UP_DOWN,
+  inout         pmod1_7_8_PWR_UP_DOWN,
   // FMC1 XM105 breakout board
   output        fmc_bob_xud1_cs_n,
   inout         fmc_bob_xud1_scl,
@@ -269,12 +269,19 @@ module system_top  #(
   assign dac_fifo_bypass  = gpio_o[60];
 
   // PMOD GPIOs
-  assign pmod0_0_1_PA_ON       = pwr_up_mask & gpio_o[61];
-  assign pmod0_4_2_TR          = pwr_up_mask & gpio_o[62];
-  assign pmod0_5_4_TX_LOAD     = pwr_up_mask & gpio_o[63];
-  assign pmod0_6_6_RX_LOAD     = pwr_up_mask & gpio_o[64];
-  assign pmod1_6_6_5V_CTRL     = pwr_up_mask & gpio_o[65];
-  assign pmod1_7_8_PWR_UP_DOWN = pwr_up_mask & gpio_o[66];
+//  assign pmod0_0_1_PA_ON       = pwr_up_mask & gpio_o[61];
+//  assign pmod0_4_2_TR          = pwr_up_mask & gpio_o[62];
+//  assign pmod0_5_4_TX_LOAD     = pwr_up_mask & gpio_o[63];
+//  assign pmod0_6_6_RX_LOAD     = pwr_up_mask & gpio_o[64];
+//  assign pmod1_6_6_5V_CTRL     = pwr_up_mask & gpio_o[65];
+//  assign pmod1_7_8_PWR_UP_DOWN = pwr_up_mask & gpio_o[66];
+
+  assign pmod0_0_1_PA_ON       = gpio_t[61] ? 1'bz : gpio_o[61];
+  assign pmod0_4_2_TR          = gpio_t[62] ? 1'bz : gpio_o[62];
+  assign pmod0_5_4_TX_LOAD     = gpio_t[63] ? 1'bz : gpio_o[63];
+  assign pmod0_6_6_RX_LOAD     = gpio_t[64] ? 1'bz : gpio_o[64];
+  assign pmod1_6_6_5V_CTRL     = gpio_t[65] ? 1'bz : gpio_o[65];
+  assign pmod1_7_8_PWR_UP_DOWN = gpio_t[66] ? 1'bz : gpio_o[66];
 
   // XUD GPIOs
   assign gpio_i[67] = fmc_bob_xud1_pmod_dip;
